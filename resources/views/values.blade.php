ee66b055ddb74c7e21904c68f858cb8c0fca4aa7
AlbertWebs <albertmuhatia@gmail.com>
2025-01-09 22:12:40 +0300

    update

---
+++ resources/views/values.blade.php
@@ -0,0 +1,38 @@
+<!--===== SERVICE AREA STARTS =======-->
+<div class="service1-section-area sp17" id="values">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="service-all-boxes-area">
+            <div class="service-boxarea per-33" data-aos="zoom-in" data-aos-duration="800">
+              <a href="#">Trust</a>
+              <div class="space40"></div>
+              <img width="100" height="100" src="{{asset('theme/assets/img/elements/45276_alliance_contractors_deal_hands_handshake_icon.png')}}" alt="">
+              <div class="space40"></div>
+              <p>
+                Building and maintaining trust with clients through transparent and ethical practices.
+              </p>
+            </div>
+
+            <div class="service-boxarea box2" data-aos="zoom-in" data-aos-duration="1000">
+              <a href="#">Professionalism</a>
+              <div class="space40"></div>
+              <img width="100" height="100" src="{{asset('theme/assets/img/elements/decision-making_8147987.png')}}" alt="">
+              <div class="space40"></div>
+              <p>Delivering services with a high degree of professionalism</p>
+            </div>
+
+            <div class="service-boxarea box3" data-aos="zoom-in" data-aos-duration="1200">
+              <a href="#">Excellence</a>
+              <div class="space66"></div>
+              <img width="100" height="100" src="{{asset('theme/assets/img/elements/premium-quality_18416084.png')}}" alt="">
+              <div class="space40"></div>
+              <p>Commitment to delivering exceptional results and service.</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--===== SERVICE AREA ENDS =======-->
